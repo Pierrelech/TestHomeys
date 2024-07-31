6c48749696a74dabb8afb4534cfd8db5871f145e
Pierrelech <pierre.lechevalier@efrei.net>
2024-07-31 17:51:29 +0200

    etape 4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <main>
-    <BaseNotification color="rouge"/>
-    <BaseNotification color="jaune"/>
-    <BaseNotification color="vert"/>
-    <BaseNotification color="bleu"/>
+    <BoutonNotif @colorGenerated="handleColorGenerated" />
+    <div class="notifications-container">
+      <BaseNotification v-for="(notification, index) in notifications" :key="index" :color="notification.color" />
+    </div>
   </main>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
   import BaseNotification from "./components/BaseNotification.vue";
+  import BoutonNotif from "./components/BoutonNotif.vue";
+
+  const notifications = ref([]);
+
+  const handleColorGenerated = (color) => {
+    notifications.value.push({ color });
+  };
 </script>
 
 <style scoped>
 
+
+
+.notifications-container {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px;
+}
+
+.notifications-container > * {
+  margin-bottom: 10px;
+}
 </style>
